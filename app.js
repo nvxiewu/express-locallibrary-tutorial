4c491175508515e58beb25b85ee5e7c1ebba2b69
JackShao <jackshao1981@gmail.com>
2020-01-17 16:46:18 +0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://JackShao:nvxiewu123@cluster0-mg3z0.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://JackShao:nvxiewu123@cluster0-mg3z0.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
